d7fc1f3ddb5601569f105fa3aa1d959741ecf416
francisco.nobre <franciscodiegonobre@gmail.com>
2023-02-07 18:50:45 +0100

    ejercicio-3

--- src/App.js
+++ src/App.js
@@ -6,45 +6,81 @@ import Navbar from "./components/Navbar";
 import ShipCard from "./components/ShipCard";
 
 function App() {
-//States
+// States
   const [shipList, setShipList] = useState([]);
   const [shipImages, setShipImages] = useState([]); 
+  const [currentPage, setCurrentPage] = useState(0)
+  const [loading, setLoading] = useState(false);
+  const [hasMoreResults, setHasMoreResults] = useState(true)
 
-// Effects: gets the list of ships from the API and puts in "shipList"
-  useEffect(() => {
-    axios.get(`https://swapi.dev/api/starships/`).then((res) => {
-      setShipList(res.data.results);
-    });
-  }, []);
+// Max number of pages for the API request  
+  const TOTAL_PAGES = 4
 
-// Gets the list of ships and builds new urls with the url ids and the img ids
-  useEffect(() => {
-    axios.get(`https://swapi.dev/api/starships/`).then((res) => {
+// Function that gets the list of ships from the API and puts in "shipList"
+const callApi = async () => {
+  setLoading(true)
+  axios.get(`https://swapi.dev/api/starships/?page=${currentPage}`)
+  .then((res) => {
+    setLoading(false);
+    setShipList(prev => [...prev, ...res.data.results]);
+  }).catch(error => {
+    console.log('An error occurred while fetching the data. Please try again later.')
+  });
+}
+
+// Function that gets the list of ships and builds new urls with the url ids and the img ids
+const callApiImages = async () => {
+  axios.get(`https://swapi.dev/api/starships/?page=${currentPage}`).then((res) => {
       const urls = res.data.results.map(element =>
         element.url);
       const idsMapped = urls.map(element => element.match(/\d+/))
       const finalUrls = idsMapped.map(element => `https://starwars-visualguide.com/assets/img/starships/${element[0]}.jpg`)
-      setShipImages(finalUrls);
+      setShipImages(prev => [...prev, ...finalUrls]);
     });
-  }, []);
+}
+
+//Effect to stop the API requests if the max number of pages is reached, else it shows a message
+useEffect(() => {
+  if (currentPage <= TOTAL_PAGES) {
+    callApi();
+    callApiImages();
+  } else {
+    setHasMoreResults(!hasMoreResults)
+  }
+}, [currentPage])
 
 // Combines both states so that I can pass shipList with the img values as props
   for (let i = 0; i < shipList.length; i++) {
     shipList[i].img = shipImages[i];
   }
 
-// Maps over shipList state and passes all of its values as props
+// Maps over shipList state and passes all of its values to as props to Shipcard
   const shipCards = shipList.map((item) => (
     <>
     <ShipCard item={item}/>
     </>
   ));
 
+// Code to apply infinite scroll to the API calls and increment the currentPage state
+useEffect(() => {
+  const intersectionObserver = new IntersectionObserver((entries) => {
+    if (entries.some((entry) => entry.isIntersecting)) {
+    setCurrentPage((prevCurrentPage) => prevCurrentPage + 1)
+  }
+});
+  intersectionObserver.observe(document.querySelector('#sentinel'));
+  return () => intersectionObserver.disconnect();
+}, []);
+
+
   return (
     <div>
       <Header />
       <Navbar />
       {shipCards}
+      {loading && <p>Loading...</p>}
+      <div id="sentinel"></div>
+      {!hasMoreResults && <p>No more starships to be displayed</p>}
     </div>
   );
 }
